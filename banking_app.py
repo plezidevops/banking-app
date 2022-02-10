b0b3473f3fc6b0a3dbd6ff9571e46a2adafb1b7a
pascalmlaurent@gmail.com <plaurent@Pascals-iMac.jeremieonline.com>
2022-02-09 22:05:45 -0500

    adding variable holding bank name

---
+++ banking_app.py
@@ -0,0 +1,13 @@
+portal_version = 1.1
+bank_name = 'Crypto Bank'
+isLogged = False
+print('Hello, welcome to ' + bank_name)
+print('You are using the banking portal version ', end=' ')
+print(portal_version)
+print('In this portal, you can handle all your banking needs.')
+print('Please enter a selection from the following menu.')
+print('Type Login to log in')
+print('Type Quit to quit')
+selection = input(' ')
+print('You have selected to: ', end=' ')
+print(selection)
